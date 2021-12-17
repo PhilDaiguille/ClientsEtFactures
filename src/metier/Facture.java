24893862ebce320a0e770bf8339ef3e80e601632
PhilDaiguille <80407535+PhilDaiguille@users.noreply.github.com>
2021-12-17 15:57:23 +0100

    Update Facture.java

--- src/metier/Facture.java
+++ src/metier/Facture.java
@@ -8,57 +8,73 @@ public class Facture
 	 * Retourne le client à qui est adressée la facture..
 	 * @return le client.
 	 */
-	
+
+	private Client nom;
+	private LocalDate date;
+	private int montant;
+	private boolean estreglee;
+
+	public Facture(Client nom, int montant, boolean estreglee, LocalDate date)
+	{
+		this.date = date;
+		this.montant = montant;
+		this.nom = nom;
+		this.estreglee = estreglee;
+
+	}
+
 	public Client getClient()
 	{
-		return null;
+		return nom;
 	}
 
 	/**
 	 * Retourne le montant de la facture.
 	 * @return le montant de la facture.
 	 */
-	
+
 	public int getMontant()
 	{
-		return 0;
+		return montant;
 	}
 
 	/**
 	 * Retourne vrai si la facture est reglée.
 	 * @return vrai ssi la facture est reglée.
 	 */
-	
+
 	public boolean estReglee()
 	{
-		return true;
+		return estreglee;
 	}
 
 	/**
 	 * Retourne la date de la facture.
 	 * @return la date de la facture.
 	 */
-	
+
 	public LocalDate getDate()
 	{
-		return null;
+		return date;
 	}
 
 	/**
 	 * Supprime la facture
 	 */
-	
+
 	public void delete()
 	{
+		this.estreglee = true;
 	}
-	
+
 	/**
 	 * Duplique la facture.
 	 * @return une copie de la facture.
 	 */
-	
+
 	public Facture copie()
 	{
-		return null;
+		Facture facture = new Facture(this.nom, this.montant, estreglee, this.date);
+		return facture;
 	}
 }
